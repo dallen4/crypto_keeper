d3535d887fe503e3b933249c68deff317d13f0f3
Nieky Allen <nieky.allen@gmail.com>
2021-02-14 01:20:43 -0600

    abstract out trade signal calc

--- main.py
+++ main.py
@@ -1,78 +1,13 @@
 import time
-import pandas as pd
-from stockstats import StockDataFrame as Sdf
 from dotenv import load_dotenv
 load_dotenv()
 
-from src.ftx_client import ftx_client
-from src.coin import get_coin, get_coin_price, get_coin_volume
-from src.coin_balance import CoinBalance
-from src.account import get_balances, get_balance
+from src.coin import get_coin_price, get_trade_signal_for_coin
+from src.account import get_balances
 from src.orders import buy_coin, sell_coin
 from src.constants import DOGE_TICKER, USD, DOGE, ETH_TICKER, BTC_TICKER
 
 convert_path = 'otc/quotes'
-
-def get_trade_signal_for_coin(ticker):
-    # fetch historical data
-    data = ftx_client.fetch_ohlcv(ticker, '1m')
-
-    # cast timestamps to readable format
-    data = [[ftx_client.iso8601(candle[0])] + candle[1:] for candle in data]
-
-    # parse data into DataFrame
-    header = ['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume']
-    coinFrame = pd.DataFrame(data, columns=header)
-
-    # impl SDF wrapper to provide more functionality
-    stock_structured_data = Sdf.retype(coinFrame)
-
-    # calculate RSI
-    stock_structured_data['rsi_14']
-    last_rsi = stock_structured_data['rsi_14'].iloc[-1]
-
-    # calculate macd and retrieve macd line and signal line
-    macd = stock_structured_data['macd']
-    macd_signal = stock_structured_data['macds']
-    timestamps = stock_structured_data['timestamp']
-    advice = []
-
-    data_length = len(macd_signal)
-    ts = timestamps[data_length - 1]
-
-    for index in range(data_length - 20, data_length):
-        if macd[index] > macd_signal[index] and macd[index - 1] <= macd_signal[index - 1]:
-            advice.append('BUY')
-        elif macd[index] < macd_signal[index] and macd[index - 1] >= macd_signal[index - 1]:
-            advice.append('SELL')
-        else:
-            advice.append('HOLD')
-
-    # positive = buy, negative = sell
-    trade_signal = 0
-
-    if last_rsi < 30:
-        trade_signal += 1
-    elif last_rsi > 70:
-        trade_signal -= 1
-
-    if advice[-1] == 'BUY':
-        trade_signal += 1
-    elif advice[-1] == 'SELL':
-        trade_signal -= 1
-
-    if trade_signal == 2:
-        print('BUY')
-    elif trade_signal == 1:
-        print('SOFT BUY')
-    elif trade_signal == 0:
-        print('HOLD')
-    elif trade_signal == -1:
-        print('SOFT SELL')
-    elif trade_signal == -2:
-        print('SELL')
-
-    return (ts, trade_signal, last_rsi, advice[-1])
 
 def run_trade_cycle(ticker):
     balances = get_balances()
--- src/coin.py
+++ src/coin.py
@@ -1,3 +1,6 @@
+import time
+import pandas as pd
+from stockstats import StockDataFrame as Sdf
 from src.ftx_client import ftx_client
 
 def get_coin(ticker):
@@ -10,3 +13,64 @@ def get_coin_price(ticker):
 def get_coin_volume(ticker):
     data = get_coin(ticker)
     return data['baseVolume']
+
+def get_trade_signal_for_coin(ticker):
+    # fetch historical data
+    data = ftx_client.fetch_ohlcv(ticker, '1m')
+
+    # cast timestamps to readable format
+    data = [[ftx_client.iso8601(candle[0])] + candle[1:] for candle in data]
+
+    # parse data into DataFrame
+    header = ['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume']
+    coinFrame = pd.DataFrame(data, columns=header)
+
+    # impl SDF wrapper to provide more functionality
+    stock_structured_data = Sdf.retype(coinFrame)
+
+    # calculate RSI
+    stock_structured_data['rsi_14']
+    last_rsi = stock_structured_data['rsi_14'].iloc[-1]
+
+    # calculate macd and retrieve macd line and signal line
+    macd = stock_structured_data['macd']
+    macd_signal = stock_structured_data['macds']
+    timestamps = stock_structured_data['timestamp']
+    advice = []
+
+    data_length = len(macd_signal)
+    ts = timestamps[data_length - 1]
+
+    for index in range(data_length - 20, data_length):
+        if macd[index] > macd_signal[index] and macd[index - 1] <= macd_signal[index - 1]:
+            advice.append('BUY')
+        elif macd[index] < macd_signal[index] and macd[index - 1] >= macd_signal[index - 1]:
+            advice.append('SELL')
+        else:
+            advice.append('HOLD')
+
+    # positive = buy, negative = sell
+    trade_signal = 0
+
+    if last_rsi < 30:
+        trade_signal += 1
+    elif last_rsi > 70:
+        trade_signal -= 1
+
+    if advice[-1] == 'BUY':
+        trade_signal += 1
+    elif advice[-1] == 'SELL':
+        trade_signal -= 1
+
+    if trade_signal == 2:
+        print('BUY')
+    elif trade_signal == 1:
+        print('SOFT BUY')
+    elif trade_signal == 0:
+        print('HOLD')
+    elif trade_signal == -1:
+        print('SOFT SELL')
+    elif trade_signal == -2:
+        print('SELL')
+
+    return (ts, trade_signal, last_rsi, advice[-1])
