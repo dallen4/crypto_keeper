6b7de6549884244f486a602e6e2eb91e47df2f67
Nieky Allen <nieky.allen@gmail.com>
2021-03-04 00:35:37 -0600

    flush out printing and iteration functionality

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import time
 from dotenv import load_dotenv
+from rich import print
+from rich.panel import Panel
+from rich.layout import Layout
+from rich.padding import Padding
 load_dotenv()
 
-from src.coin import get_coin_price, get_trade_signal_for_coin
 from src.account import get_balances
-from src.orders import buy_coin, sell_coin
+from src.trade import run_trade_cycle
 from src.constants import DOGE_TICKER, USD, DOGE, ETH_TICKER, BTC_TICKER
 
-convert_path = 'otc/quotes'
+run = True
+balance_list = []
 
-def run_trade_cycle(ticker):
-    balances = get_balances()
+def check_coins():
+    (balances, total_balance) = get_balances()
+
+    balance_list.append(total_balance)
+    account_output = '[bold]Total Account Balance:[/bold] [blue]' + str(total_balance) + '[/blue]'
+
+    diff_since_start = total_balance - balance_list[0]
+
+    color = 'green'
+
+    if diff_since_start < 0:
+        color = 'red'
+
+    account_output += '\n[bold]Balance Change Since Start:[/bold] [' + color + ']' + str(diff_since_start) + '[/' + color + ']'
+
+    account_panel = Panel(account_output)
+
     usd_balance = balances[USD]
 
     max_buy = 0
@@ -18,37 +37,36 @@ def run_trade_cycle(ticker):
     if usd_balance.amountUsd > 20:
         max_buy = 20
 
-    (timestamp, signal, rsi, macd_advice) = get_trade_signal_for_coin(ticker)
-    coin_price = get_coin_price(ticker)
-
-    print(ticker + ' ' + timestamp)
-    print(coin_price)
-    print('RSI: ' + str(rsi))
-    print('MACD Signal: ' + macd_advice)
-    print(signal)
-
-    if signal == 2:
-        if max_buy > 0:
-            amount_to_buy = max_buy / coin_price
-            buy_coin(ticker, amount_to_buy)
-            print('BOUGHT ' + str(amount_to_buy) + ' ' + ticker)
-        else:
-            print('MISSED BUY')
-    elif signal == -2:
-        coin_balance = balances[ticker.split('/')[0]]
-        amount_to_sell = coin_balance.amount * .25
-        sell_coin(ticker, amount_to_sell)
-        print('SOLD ' + str(amount_to_sell) + ' ' + ticker)
-
-    print('------------------------')
-
-run = True
-
-while run == True:
-
     tickers = [DOGE_TICKER, ETH_TICKER, BTC_TICKER]
+    summaries = []
 
-    run_trade_cycle(DOGE_TICKER)
+    for ticker in tickers:
+        trade_summary = run_trade_cycle(ticker, max_buy)
+        summaries.append(trade_summary)
 
-    time.sleep(60)
-    # break
+    cycle_layout = Layout()
+
+    cycle_layout.split(
+        Layout(account_panel, name='top'),
+        Layout(name='bottom')
+    )
+
+    cycle_layout['top'].size = 5
+    cycle_layout['bottom'].size = 10
+
+    cycle_layout['bottom'].split(
+        Layout(Panel(Padding(summaries[0], 1))),
+        Layout(Panel(Padding(summaries[1], 1))),
+        Layout(Panel(Padding(summaries[2], 1))),
+        direction='horizontal'
+    )
+    print(cycle_layout)
+
+def main():
+    while run == True:
+        check_coins()
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
--- src/account.py
+++ src/account.py
@@ -5,13 +5,16 @@ def get_balances():
     balances_data = ftx_client.fetch_balance()
     balances_data = balances_data['info']['result']
     balances = {}
+    total_balance = 0
 
     for coin in balances_data:
-        curr_balance = CoinBalance(coin['coin'], coin['total'], coin['usdValue'])
-        balances[coin['coin']] = curr_balance
+        symbol = coin['coin']
+        curr_balance = CoinBalance(symbol, coin['total'], coin['usdValue'])
+        balances[symbol] = curr_balance
+        total_balance += curr_balance.amountUsd
 
-    return balances
+    return (balances, total_balance)
 
 def get_balance(coin):
-    balances = get_balances()
+    (balances) = get_balances()
     return balances[coin]
